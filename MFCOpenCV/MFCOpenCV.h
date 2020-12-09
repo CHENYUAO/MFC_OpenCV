fb1967e35ecd9b81aa395e0e4c6d24562628bc55
m15927509890@163.com <m15927509890@163.com>
2020-12-09 17:21:15 +0800

    添加项目文件。

---
+++ MFCOpenCV/MFCOpenCV.h
@@ -0,0 +1,32 @@
+﻿
+// MFCOpenCV.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCOpenCVApp:
+// 有关此类的实现，请参阅 MFCOpenCV.cpp
+//
+
+class CMFCOpenCVApp : public CWinApp
+{
+public:
+	CMFCOpenCVApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCOpenCVApp theApp;
---
+++ MFCOpenCV/MFCOpenCVDlg.h
@@ -0,0 +1,43 @@
+﻿
+// MFCOpenCVDlg.h: 头文件
+//
+
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include <string>
+
+// CMFCOpenCVDlg 对话框
+class CMFCOpenCVDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCOpenCVDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCOPENCV_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+
+public:
+	cv::Mat image;
+};
